e1c94d8205fc2bbe8b78e46de9061b1e08585d5b
Fokoue22 <fokouethomasdylan@gmail.com>
2026-04-18 19:37:40 -0400

    feat(m4): add NotificationService with reminders and weekly summaries

--- app/application/__init__.py
+++ app/application/__init__.py
@@ -1,10 +1,12 @@
 from .m1_services import ConflictError, DisponibiliteService, NotFoundError, PlateauService
+from .m4_services import NotificationService
 from .m2_services import ReservationService
 
 __all__ = [
 	"PlateauService",
 	"DisponibiliteService",
 	"ReservationService",
+	"NotificationService",
 	"NotFoundError",
 	"ConflictError",
 ]
